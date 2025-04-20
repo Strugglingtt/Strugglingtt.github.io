48d1557a2dd0d785613a17f687aaf9fd6d6bc8c2
Strugglingtt <2434787541@qq.com>
2025-04-20 09:26:54 +0800

    Site updated: 2025-04-20 09:26:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/18/Learn-Markdown/","2025/04/18/hello-world/","2025/04/19/Prompt/","2025/04/19/project/"];function toRandomPost(){
+var posts=["2025/04/18/Learn-Markdown/","2025/04/19/project/","2025/04/19/Prompt/","2025/04/18/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
