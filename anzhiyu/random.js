06bdb3515638ac60d3cb0ec0aec34bcc4fc53e59
Strugglingtt <2434787541@qq.com>
2025-04-22 15:21:43 +0800

    Site updated: 2025-04-22 15:21:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/18/Learn-Markdown/","2025/04/19/Prompt/","2025/04/19/project/","2025/04/22/test2/","2025/04/21/work/","2025/04/22/test/"];function toRandomPost(){
+var posts=["2025/04/18/Learn-Markdown/","2025/04/19/project/","2025/04/22/test/","2025/04/21/work/","2025/04/19/Prompt/","2025/04/22/test2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
